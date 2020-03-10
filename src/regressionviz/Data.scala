37c5326fb78c4ba1a4ade74570fb36f5c50ed7e6
Hölttä Matti <holttam2@vdiubuntu069.org.aalto.fi>
2020-03-10 19:37:01 +0200

    Added intial methods to data-class. Method implementations missing.

--- src/regressionviz/Data.scala
+++ src/regressionviz/Data.scala
@@ -1,5 +1,15 @@
 package regressionviz
 
-class Data {
+class Data (val file: String) {
   
+  private var storedData = ???
+  // Method calls either loadCSV or loadJSON based on filetype
+  def loadFile = ???
+  
+  private def loadCSV = ???
+  
+  private def loadJSON = ???
+  
+  // Returns the data loaded
+  def getData = this.storedData
 }
