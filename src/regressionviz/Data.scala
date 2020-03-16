d0981e2bf19dff962c537039d13bfd0bd637547b
Hölttä Matti <holttam2@vdiubuntu069.org.aalto.fi>
2020-03-16 11:24:01 +0200

    FIrst version of readCSV method in Data.scala made. Method untested, lacks exception handling and unit tests are not made. Noted at the same time Breeze library was not working as intended. Wrong Scala version when assembling JAR-file. Fixed issue.

--- src/regressionviz/Data.scala
+++ src/regressionviz/Data.scala
@@ -1,14 +1,48 @@
 package regressionviz
 
+// Importing data type fo be used
 import breeze.linalg.{DenseMatrix, DenseVector}
+
+// Importing file reading tools
+import scala.io.Source
 
 class Data (val file: String) {
   
-  private var storedData = ???
+  private var storedData: Option[DenseMatrix] = None
+  private var dataHeader: Option[Array[String]] = None
+  
   // Method calls either loadCSV or loadJSON based on filetype
   def loadFile = ???
   
-  private def loadCSV = ???
+  private def loadCSV(file: String) = {
+    val loader = Source.fromFile(file).getLines()
+    val header = loader.take(1).toString.split(";").map(_.trim)
+    
+    // Data from file to array of arrays
+    // TODO add try and except here, even if file and data exists, toDouble can cause errors
+    val content = loader.toArray
+                         .map(_.split(";"))
+                         .map(_.map(_.trim))
+                         .map(_.map(_.toDouble))
+                         
+    val lenght = content.size
+    
+    val width = if(lenght > 0) content(1).size else 0
+    
+    val sameWidth = content.forall(_.size == width)
+    
+    if( !sameWidth || lenght == 0 || width == 0) {
+      // TODO raise some exception
+    }
+    
+    // Creating DenseMatrix
+    val data = new DenseMatrix(lenght, width, content.flatten)
+ 
+    
+    
+    // Returning headers and data in breeze dense matrix
+    (header, data)
+  }
   
   private def loadJSON = ???
   
@@ -16,4 +50,8 @@ class Data (val file: String) {
   
   // Returns the data loaded
   def getData = this.storedData
+  
+  // Returns data header
+  def getHeader = this.dataHeader
+  
 }
